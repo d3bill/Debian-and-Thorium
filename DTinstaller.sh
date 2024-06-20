8244f7084ef6dc819cdf0e2e51554e930d63af0a
d3bill <60115123+d3bill@users.noreply.github.com>
2024-06-19 21:41:37 -0400

    Create DTinstaller.sh

---
+++ DTinstaller.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Original script copied from https://github.com/josfaber/debian-kiosk-installer/blob/master/kiosk-installer.sh
+# add thorium browser repositories
+sudo rm -fv /etc/apt/sources.list.d/thorium.list && \
+sudo wget --no-hsts -P /etc/apt/sources.list.d/ \
+http://dl.thorium.rocks/debian/dists/stable/thorium.list && \
+sudo apt update
+
+sudo apt install thorium-browser
+# get software
+apt install xorg openbox lightdm locales -y
+
+# dir
+mkdir -p /home/kiosk/.config/openbox
+
+# create group
+groupadd kiosk
+
+# create user if not exists
+id -u kiosk &>/dev/null || useradd -m kiosk -g kiosk -s /bin/bash 
+
+# rights
+chown -R kiosk:kiosk /home/kiosk
+
+# create config
+if [ -e "/etc/lightdm/lightdm.conf" ]; then
+  mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
+fi
+cat > /etc/lightdm/lightdm.conf << EOF
+[SeatDefaults]
+autologin-user=kiosk
+user-session=openbox
+EOF
+
+# create autostart
+if [ -e "/home/kiosk/.config/openbox/autostart" ]; then
+  mv /home/kiosk/.config/openbox/autostart /home/kiosk/.config/openbox/autostart.backup
+fi
+cat > /home/kiosk/.config/openbox/autostart << EOF
+#!/bin/bash
+
+#
+
+while :
+do
+  xrandr --auto
+  thorium-browser \
+    --no-first-run \
+    --start-maximized \
+    --disable \
+    --disable-translate \
+    --disable-infobars \
+    --disable-suggestions-service \
+    --disable-session-crashed-bubble \
+    
+    
+  sleep 5
+done &
+EOF
+
+echo "Done!"
